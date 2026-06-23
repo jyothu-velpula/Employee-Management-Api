62c45130b7f45383b8c917ab39a8af0ed4f23031
jyothu-velpula <125673020+jyothu-velpula@users.noreply.github.com>
2026-06-23 07:20:58 +0000

    fe env

--- frontend/src/EmployeeList.js
+++ frontend/src/EmployeeList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL =
+const API_URL = process.env.REACT_APP_API_URL || 
   "https://musical-bassoon-7qq965g6pvx2wqg9-5000.app.github.dev";
 
 function EmployeeList() {
